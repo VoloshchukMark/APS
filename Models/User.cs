bfdb7a40b9f1fbc7966f4119be96547fe68eee96
bel-dot <artemonchannel2005@gmail.com>
2025-09-16 22:31:32 +0300

    Add code for User class, implement prototype pattern

--- Models/User.cs
+++ Models/User.cs
@@ -1,7 +1,20 @@
 namespace APS.Models
 {
-    public class User
+    public class User 
     {
+        private string name;
         
+        public User(string name) 
+        {
+            this.name = name;
+        }
+        
+        public User(User user) {
+            this.name = user.name;
+        }
+        
+        public User Clone() {
+            return new User(this);
+        }
     }
 }
