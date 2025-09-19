3daf95d3b9d8d4415ace32c239bde2997fef856a
bel-dot <artemonchannel2005@gmail.com>
2025-09-19 09:24:27 +0300

    Add implementation for Lesson class

--- Models/Lesson.cs
+++ Models/Lesson.cs
@@ -1,7 +1,73 @@
+using System;
+using System.Data.Common;
+using System.Runtime.CompilerServices;
+
 namespace APS.Models
 {
-    public class Lesson
+    // Lessons
+    public abstract class Lesson
     {
+        protected string Name;
+        public Lesson(string name)
+        {
+            Name = name;
+        }
+        public abstract void StartLesson();
+    }
+    
+    public class VideoLesson : Lesson 
+    {
+        public VideoLesson(string name): base(name) { }
         
+        public override void StartLesson()
+        {
+            Console.WriteLine("Tung tung tung tung tung sahur");
+        }
+    }
+
+    public class TextLesson : Lesson 
+    {
+        public TextLesson(string name): base(name) { }
+        
+        public override void StartLesson()
+        {
+            Console.WriteLine("This is a text lesson");
+        }
+    }
+    
+    // Factories
+    public abstract class LessonFactory
+    {
+        public abstract Lesson createLesson(string name);
+    }
+    
+    public class VideoFactory : LessonFactory
+    {
+        public override Lesson createLesson(string name)
+        {
+            return new VideoLesson(name);
+        } 
+    }
+
+    public class TextFactory : LessonFactory
+    {
+        public override Lesson createLesson(string name)
+        {
+            return new TextLesson(name);
+        } 
+    }
+    
+    public class LessonService
+    {
+        private LessonFactory Factory;
+        public LessonService(LessonFactory factory)
+        {
+            Factory = factory;
+        }
+        
+        public Lesson createLesson(string name)
+        {
+            return Factory.createLesson(name);
+        }
     }
 }
