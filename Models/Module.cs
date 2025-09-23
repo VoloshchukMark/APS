02fe9fdcfc09157f29463f9f6d04f00277b610f8
VoloshchukMark <voloshchuk.mark@chnu.edu.ua>
2025-09-24 00:52:22 +0300

    Add code for Module class, implement the Factory method

--- Models/Module.cs
+++ Models/Module.cs
@@ -1,7 +1,69 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace APS.Models
 {
-    public class Module
+    abstract class Creator()
     {
-        
+        public abstract IModule FactoryMethod();
+
+        private string Name;
+        private int MaxGrade;
+
+
+        public string DisplayInformation()
+        {
+            var module = FactoryMethod();
+            var result = "Module: " + Name + "\n" +
+                         "Max Grade: " + MaxGrade + "\n" +
+                         module.GetList();
+
+            return result;
+        }
     }
+
+    class TestModuleCreator : Creator
+    {
+        public override IModule FactoryMethod()
+        {
+            return new TestModule();
+        }
+    }
+
+    class TestModule : IModule
+    {
+        public List<(string Name, int Grade)> tests;
+
+        public TestModule()
+        {
+            tests = new List<(string Name, int Grade)>
+            {
+                ("Test 1", 5),
+                ("Test 2", 5),
+                ("Test 3", 10)
+            };
+        }
+
+
+        public List<(string Name, int Grade)> GetList()
+        {
+            return tests;
+        }
+    }
+
+    public interface IModule
+    {
+        List<(string Name, int Grade)> GetList();
+    }
+
+    class Module
+    {
+        public void ModuleCode(Creator creator)
+        {
+            Console.WriteLine(creator.DisplayInformation());
+        }
+    }
+
+
 }
