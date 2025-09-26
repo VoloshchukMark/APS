02b7b850eb6c2375fd30b3cf87ae97a55df46348
bel-dot <artemonchannel2005@gmail.com>
2025-09-26 08:55:53 +0300

    Implement Test class with Object Pool

--- Models/Test.cs
+++ Models/Test.cs
@@ -1,7 +1,78 @@
+using System;
+using System.Collections.Generic;
+
 namespace APS.Models
 {
     public class Test
     {
+        private string Name;
+        private List<Question> Questions;
         
+        public string GetName() { return Name; }
+        public void SetName(string name) { Name = name; }
+        
+        public List<Question> GetQuestions() { return Questions; }
+        public void SetQuestions(List<Question> questions) { Questions = questions; }
+
+        public Test()
+        {
+            Name = "";
+            Questions = [];
+        }
+
+        public Test(string name)
+        {
+            Name = name;
+            Questions = [];
+        }
+
+        public Test(string name, List<Question> questions)
+        {
+            Name = name;
+            Questions = questions;
+        }
+    }
+    
+    public static class TestPool
+    {
+        private static List<Test> AvailableTests = [];
+        private static List<Test> UsedTests = [];
+        
+        public static Test GetTest()
+        {
+            lock(AvailableTests)
+            {
+                if (AvailableTests.Count == 0)
+                {
+                    var test = new Test();
+                    UsedTests.Add(test);
+                    return test;
+                }
+                else
+                {
+                    var test = AvailableTests[0];
+                    AvailableTests.RemoveAt(0);
+                    UsedTests.Add(test);
+                    return test;
+                }
+            }
+        }
+        
+        public static void ReleaseTest(Test test)
+        {
+            CleanTest(test);
+
+            lock(AvailableTests)
+            {
+                UsedTests.Remove(test);
+                AvailableTests.Add(test);
+            }
+        }
+        
+        private static void CleanTest(Test test)
+        {
+            test.SetName("");
+            test.SetQuestions([]);
+        }
     }
 }
