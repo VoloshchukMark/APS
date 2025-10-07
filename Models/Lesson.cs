41613c35dd3e4b41d21f93c45c8aee03f701ee8e
bel-dot <artemonchannel2005@gmail.com>
2025-10-07 21:08:20 +0300

    Implement bridge pattern into Lesson

--- Models/Lesson.cs
+++ Models/Lesson.cs
@@ -4,34 +4,70 @@ using System.Runtime.CompilerServices;
 
 namespace APS.Models
 {
+    // Bridge Implementors
+    public interface ILessonImplementor
+    {
+        void PlayVideo(string name);
+        
+        void ShowText(string name);
+    }
+
+    public class ConsoleLessonImplementor : ILessonImplementor
+    {
+        public void PlayVideo(string name)
+        {
+            Console.WriteLine($"[Console] Playing video: {name}");
+        }
+
+        public void ShowText(string name)
+        {
+            Console.WriteLine($"[Console] Showing text: {name}");
+        }
+    }
+
+    public class CdnLessonImplementor : ILessonImplementor
+    {
+        public void PlayVideo(string name)
+        {
+            Console.WriteLine($"[CDN] Streaming video: {name}");
+        }
+
+        public void ShowText(string name)
+        {
+            Console.WriteLine($"[CDN] Rendering text lesson: {name}");
+        }
+    }
+    
     // Lessons
     public abstract class Lesson
     {
         protected string Name;
-        public Lesson(string name)
+        protected ILessonImplementor Implementor;
+        public Lesson(string name, ILessonImplementor implementor)
         {
             Name = name;
+            Implementor = implementor;
         }
         public abstract void StartLesson();
     }
     
     public class VideoLesson : Lesson 
     {
-        public VideoLesson(string name): base(name) { }
+        public VideoLesson(string name, ILessonImplementor implementor): base(name, implementor) { }
         
         public override void StartLesson()
         {
-            Console.WriteLine("Tung tung tung tung tung sahur");
+            Implementor.PlayVideo(Name);
         }
     }
 
     public class TextLesson : Lesson 
     {
-        public TextLesson(string name): base(name) { }
+        public TextLesson(string name, ILessonImplementor implementor): base(name, implementor) { }
         
         public override void StartLesson()
         {
-            Console.WriteLine("This is a text lesson");
+            Implementor.ShowText(Name);
         }
     }
     
